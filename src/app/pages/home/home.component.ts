54de36bff44e9163aeeb6c797d4351c111d22dc2
porman193 <porman193@gmail.com>
2025-06-17 00:05:48 -0500

    Resolucion

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,67 @@
+import { Task } from './../../shared/models/task.model';
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { TaskService } from 'src/app/shared/services/task.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  tasks$!: Observable<Task[]>;
+
+  newTaskCtrl = new FormControl('',{
+    nonNullable:true,
+    validators:[
+      Validators.required,
+      Validators.pattern(/^(?!.* {3,})(?! )[^\s](.*[^\s])?$/)
+    ]
+  });
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.tasks$ = this.taskService.tasks$;
   }
 
+  addTask(event: Event) {
+    if(this.newTaskCtrl.valid){
+      const input = event.target as HTMLInputElement;
+      let value = input.value;
+      let newTask: Task = {
+        id:Date().toString(),
+        title:value,
+        completed:false
+      };
+      this.taskService.addTask(newTask);
+      input.value = '';
+    }
+  }
+
+  removeTask(id: string) {
+    this.taskService.removeTask(id);
+  }
+
+  toggleTaskCompletion(id: string) {
+    this.taskService.toggleTaskState(id);
+  }
+
+  changeEditState(id: string) {
+    this.taskService.toggleEditState(id);
+  }
+
+  updateTask(task: Task,event:Event) {
+      const input = event.target as HTMLInputElement;
+      let value = input.value;
+      const updateTask = {
+        ...task,
+        title: value,
+        edit:false
+      }
+      this.taskService.updateTask(updateTask);
+  }
 }
--- src/app/shared/services/task.service.ts
+++ src/app/shared/services/task.service.ts
@@ -1,10 +1,68 @@
-import { Injectable,signal } from '@angular/core';
 
+import { BehaviorSubject, Observable} from 'rxjs';
+import { Task } from '../models/task.model';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  tasks = signal<Task[]>([]);
 
+  TASKS_KEY = 'mydayapp-angular';
+  private tasksSubject = new BehaviorSubject<Task[]>(this.loadTasks());
+  tasks$: Observable<Task[]> = this.tasksSubject.asObservable();
+
+  private loadTasks(): Task[] {
+    const data = localStorage.getItem(this.TASKS_KEY);
+    return data ? JSON.parse(data) : [];
+  }
+
+  private saveTasks(tasks: Task[]) {
+    localStorage.setItem(this.TASKS_KEY, JSON.stringify(tasks));
+  }
+
+  get tasks(): Task[] {
+    return this.tasksSubject.value;
+  }
+
+  addTask(task: Task) {
+    const updated = [...this.tasksSubject.value, task];
+    this.tasksSubject.next(updated);
+    this.saveTasks(updated);
+  }
+
+  updateTask(updatedTask: Task) {
+    const updated = this.tasksSubject.value.map(task =>
+      task.id === updatedTask.id ? updatedTask : task
+    );
+    this.tasksSubject.next(updated);
+    this.saveTasks(updated);
+  }
+
+  toggleTaskState(id:string){
+    const updated = this.tasksSubject.value.map(task =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    this.tasksSubject.next(updated);
+    this.saveTasks(updated);
+  }
+
+  removeTask(id: string) {
+    const updated = this.tasksSubject.value.filter(task => task.id !== id);
+    this.tasksSubject.next(updated);
+    this.saveTasks(updated);
+  }
+
+  setTasks(tasks: Task[]) {
+    this.tasksSubject.next(tasks);
+    this.saveTasks(tasks);
+  }
+
+  toggleEditState(id: string) {
+    const updated = this.tasksSubject.value.map(task =>
+      task.id === id ? { ...task, edit: true } : { ...task, edit: false }
+    );
+    this.tasksSubject.next(updated);
+    this.saveTasks(updated);
+  }
 }
